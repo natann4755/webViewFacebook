2b06293b0dd425dabf1436f2812691ee808ae9e4
natann <natann4755@gmail.com>
2020-06-22 06:34:10 -0700

    first commit

---
+++ app/src/main/java/com/example/mywebview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mywebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView myWebView = new WebView(this);
+
+        setContentView(myWebView);
+        myWebView.loadUrl("https://www.facebook.com/pg/Ice-102826024821865/posts/?ref=page_internal");
+    }
+}
